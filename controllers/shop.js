ad12071f69bef69c270c4c5632aacc61f8b9a980
snadi043 <snadi043@uottawa.ca>
2025-03-09 16:44:16 -0400

    Replacing the fetchAll() method which is previously used by the product model with the findAll() method provided by the Sequelize to getIndex and getProducts endpoints work again with the help of sequelize.

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,12 +4,12 @@ const Cart = require('../models/cart');
 // GET Request to handle the display of the products functionality using the controllers in MVC pattern.
 // Implementing using the database and promises.
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll().then(([rows, databaseMetaData]) => {
-      res.render('shop/product-list', {
-        prods: rows,
-        pageTitle: 'All Products',
-        path: '/products'
-      });
+  Product.findAll().then(products => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'All Products',
+      path: '/products'
+    });
     }).catch(err => console.log(err));
 };
 
@@ -32,9 +32,9 @@ exports.getIndex = (req, res, next) => {
   // If you execute the code below in the line 32 you get the result of two arrays which is nested into an array.
   // So, to avoid it here using the array destructing to handle the individual array values.
   // Product.fetchAll().then(result => console.log(result)).catch(err => console.log(err));
-  Product.fetchAll().then(([rows, databaseMetaData]) => {
+  Product.findAll().then(products => {
     res.render('shop/index',{
-      prods: rows,
+      prods: products,
       pageTitle: 'Shop',
       path: '/'
     });
